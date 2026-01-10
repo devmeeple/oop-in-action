d1b982a89b6d36023db0b9dce3887ba7ad491063
TaeGeun Jang <devmeeple@gmail.com>
2026-01-11 06:40:07 +0900

    테스트 코드에 사용하는 Mockito  추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,16 @@ repositories {
     mavenCentral()
 }
 
+val mockitoVersion = "5.21.0"
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:$mockitoVersion") // Mock 객체 생성
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion") // JUnit5와 Mockito 연결
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
